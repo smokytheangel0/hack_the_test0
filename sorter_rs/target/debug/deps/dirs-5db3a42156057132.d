5139ec87387bf1369e02e8215ac349e1182d7cce
smokytheangel0 <j.m.a.kierstead@gmail.com>
2018-11-23 15:50:46 -0800

    adds folders for other langs

---
+++ sorter_rs/target/debug/deps/dirs-5db3a42156057132.d
@@ -0,0 +1,7 @@
+/home/wiggles/Desktop/hack_the_test0/sorter/target/debug/deps/libdirs-5db3a42156057132.rlib: /home/wiggles/.cargo/registry/src/github.com-1ecc6299db9ec823/dirs-1.0.4/src/lib.rs /home/wiggles/.cargo/registry/src/github.com-1ecc6299db9ec823/dirs-1.0.4/src/lin.rs /home/wiggles/.cargo/registry/src/github.com-1ecc6299db9ec823/dirs-1.0.4/src/unix.rs
+
+/home/wiggles/Desktop/hack_the_test0/sorter/target/debug/deps/dirs-5db3a42156057132.d: /home/wiggles/.cargo/registry/src/github.com-1ecc6299db9ec823/dirs-1.0.4/src/lib.rs /home/wiggles/.cargo/registry/src/github.com-1ecc6299db9ec823/dirs-1.0.4/src/lin.rs /home/wiggles/.cargo/registry/src/github.com-1ecc6299db9ec823/dirs-1.0.4/src/unix.rs
+
+/home/wiggles/.cargo/registry/src/github.com-1ecc6299db9ec823/dirs-1.0.4/src/lib.rs:
+/home/wiggles/.cargo/registry/src/github.com-1ecc6299db9ec823/dirs-1.0.4/src/lin.rs:
+/home/wiggles/.cargo/registry/src/github.com-1ecc6299db9ec823/dirs-1.0.4/src/unix.rs:
---
+++ sorter_rs/target/debug/sorter-52cd33b2c6077756.d
@@ -0,0 +1,1 @@
+/home/wiggles/Desktop/hack_the_test0/sorter/target/debug/sorter-52cd33b2c6077756: /home/wiggles/Desktop/hack_the_test0/sorter/src/main.rs
